64be6f6a395d2aba33d94ef68639fb48a14ab484
zhengzhiyong <zhengzhiyong@jumore.com>
2019-04-28 17:48:46 +0800

    dubbo服务广播模式和zookeeper模式暴露服务demo

--- dubbo-service/dubbo/dubbo-service/src/main/java/com/zy/dubbo/service/Provider.java
+++ dubbo-service/dubbo/dubbo-service/src/main/java/com/zy/dubbo/service/Provider.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @date 2019/2/18.
  */
 public class Provider {
+//    如果使用zookeeper方式暴露服务地址，在需要先开启动zookeeper服务
     public static void main(String[] args) throws  Exception{
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:applicationContext-dubbo.xml"});
         context.start();
